0f1cf8966328f1347dd54941540518b0ea5aa041
MichaelLin12 <michael.lin2032@gmail.com>
2022-02-07 21:08:19 -0600

    added some routes to web app

--- solutions/src/components/App.js
+++ solutions/src/components/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import {Routes, Route} from 'react-router-dom';
-import Home from './Home';
-import Login from './Login';
-import SignUp from './SignUp';
+import Home from './Entry/Home';
+import Login from './Entry/Login';
+import SignUp from './Entry/SignUp';
 import ProtectedRoute from './ProtectedRoute';
+import { authentication as auth} from "../firebase.js";
+import {Navigate} from "react-router-dom";
+import Market from './Market';
 
 export default class App extends Component {
+
+  /**
+   * this method returns the current user and is used in
+   * the protected route component to test if a user does exist
+   * @returns the current user
+   */
+  checkAuthentication(){
+    const user = auth.currentUser;
+    return user;
+  }
+
+  /**
+   * the render method is a compulsory method in react
+   * @returns what is to be rendered as html on webpage
+   */
   render() {
     return (
     <div className="App">
-      <h1>Hello World</h1>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>The Page you are looking for does not exist</p>
-            </main>
-          }
-        />
+        <Route path="/market" 
+        element={
+        <ProtectedRoute func={this.checkAuthentication}>
+          <Market/>
+        </ProtectedRoute>}/>
+        <Route path="*" element={<Navigate to="/"/>}/>
       </Routes>
     </div>
     );
--- solutions/src/components/ProtectedRoute.js
+++ solutions/src/components/ProtectedRoute.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import { getAuth } from "firebase/auth";
-import {Outlet} from 'react-router-dom';
-import Home from './Home';
+import {Navigate} from 'react-router-dom'
 
-const auth = getAuth();
-const user = auth.currentUser;
 export default function ProtectedRoute(props) {
-    if(user){
-        return <Outlet/>
-    }
-    else{
-        return <Home/>
-    }
+    console.log("Private route in protected route")
+    const res = props.func();
+    return (res)? props.children:<Navigate to="/"/>
 }
