b35b20d108b87cdbc45535df2ff912423490f32a
MichaelLin12 <michael.lin2032@gmail.com>
2022-02-12 22:23:38 -0600

    add appbar to marketplace

--- solutions/src/components/Market.js
+++ solutions/src/components/Market.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {AppBar, Toolbar} from '@mui/material'
 
 export default function Market() {
   return (
   <div>
+      <AppBar position="sticky">
+        <Toolbar>Hello</Toolbar>
+      </AppBar>
       This is the MarketPlace
   </div>
   );
