e4e22b4d354a860a300a86ae748a0cfd68552ca4
MichaelLin12 <michael.lin2032@gmail.com>
2022-02-14 19:24:01 -0600

    worked on the market component

--- solutions/src/components/Market.js
+++ solutions/src/components/Market.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import {AppBar, Toolbar} from '@mui/material'
+import {AppBar, Toolbar, Drawer, IconButton, Typography, Button} from '@mui/material'
+import '../css/Market.css';
 
 export default function Market() {
   return (
-  <div>
-      <AppBar position="sticky">
-        <Toolbar>Hello</Toolbar>
+  <div className='market'>
+    <div className='appbar'>
+    <AppBar position="static" sx={{height:'100%'}}>
+        <Toolbar>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
       </AppBar>
-      This is the MarketPlace
+    </div>
+    <div className='item2'>
+      <div className='navbar'>
+        <Drawer anchor='left' variant='permanent'>
+          Market
+          Cart
+        </Drawer>
+      </div>
+      <div className='content'>
+        This is the content space
+      </div>
+    </div>
   </div>
   );
 }
